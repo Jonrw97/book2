57fa5503f8fa8f4359210464a38886ce033115e1
Jonrw97 <jonrw97@gmail.com>
2022-04-25 19:50:27 +0200

    book2

--- ex38/dbtest1.sql
+++ ex38/dbtest1.sql
@@ -3,3 +3,17 @@ create table users(
     email_address varchar(255) not null,
     name varchar(255)
 );
+
+create table people(
+    id integer primary key,
+    name text,
+    age integer
+);
+
+insert into users(id, email_address) values(1, 'jon@gmail.com');
+select * from users;
+select id,name from users where id=1;
+
+select id, name, age from people;
+
+drop table [schema_name.]people;
